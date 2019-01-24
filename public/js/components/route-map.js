6bd0904c4836c04b4c75a7e8cb4fbc8604ea336c
Nick Holloway <nick.holloway@pyrites.org.uk>
2019-01-24 19:08:36 +0000

    Add fullscreen control to map

--- public/js/components/route-map.js
+++ public/js/components/route-map.js
@@ -1,7 +1,7 @@
 /* global L */
 
 function mounted() {
-  this.map = L.map('route-map');
+  this.map = L.map('route-map', { fullscreenControl: { position: 'topright' } });
 
   this.tileLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | &copy; <a href="https://carto.com/attributions">CARTO</a>',
