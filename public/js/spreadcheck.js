24297299ed45ebdea99baa0fceaafead3ad6eab6
Nick Holloway <nick.holloway@pyrites.org.uk>
2019-03-23 10:02:24 +0000

    Add missing semicolon

--- public/js/spreadcheck.js
+++ public/js/spreadcheck.js
@@ -2,7 +2,7 @@ try {
   eval('const i = {}; const j = {...i};');
 }
 catch (e) {
-  const script = document.createElement("script")
+  const script = document.createElement("script");
   script.src = 'js/unsupported.js';
   document.body.appendChild(script);
 }
