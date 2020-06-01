b6f47967e0615d3145e717ff5722680e8a2bb99b
Tobias <tobiasmark86@gmail.com>
2020-06-01 22:08:20 +0200

    Added some cleanup of create bounding box

--- public/js/poi.js
+++ public/js/poi.js
@@ -32,6 +32,59 @@ function isInverseBearing(from, to, expected) {
     return Math.abs(bearing - expected) > 120;
 }
 
+// douglas-pecker simplification of input route
+function doSimplify(points, distance) {
+    // convert format
+    let xy = points.map((p) => {return {x: p.lat, y: p.lon};});
+    let simple = L.LineUtil.simplify(xy, 0.0001);
+    // convert back
+    return simple.map((p) => {return {lat: p.x, lon: p.y};});
+}
+
+function ccw(A,B,C) {
+    return (C.lon-A.lon) * (B.lat-A.lat) > (B.lon-A.lon) * (C.lat-A.lat);
+}
+
+//Return true if line segments AB and CD intersect
+// Source: https://stackoverflow.com/questions/3838329/how-can-i-check-if-two-segments-intersect
+function intersect(A,B,C,D) {
+    return ccw(A,C,D) !== ccw(B,C,D) && ccw(A,B,C) !== ccw(A,B,D);
+}
+
+/**
+ * turns tend to create loops inside the simple offset logic; While
+ * inverseBearing() filter some it can't catch all.
+ *
+ * So simply search for points where the line intersects itself and cut
+ * those.
+ */
+function removeLoops(points, distance) {
+    const distanceLimit = (4 * distance) * 1000; // meters
+    for(let pos = 0; pos < points.length; pos++) {
+	// select a segment
+	let start = points[pos];
+	let startLatLng = L.latLng(start.lat, start.lon);
+	let next = points[pos+1];
+	let previous = next;
+	// now search for loops
+	for(let i = pos + 2, cnt = 0; i < points.length; i++, cnt++) {
+	    let current = points[i];
+	    // stop loop once we far away from starting point
+	    if(cnt > 15 || startLatLng.distanceTo([current.lat, current.lon]) > distanceLimit) {
+		break;
+	    }
+	    if(intersect(start, next, current, previous)) {
+		// remove all elements involved in the loop
+		points.splice(pos + 1, cnt + 1);
+		break;
+	    }
+	    previous = current;
+	}
+    }
+    return points;
+}
+
+
 /**
  * Convert a Route into a "stream" of width distance*2 to get
  * a bounding box for poi searching
@@ -39,7 +92,10 @@ function isInverseBearing(from, to, expected) {
 export function getBoundingBox(points, distance) {
     let list1 = [];
     let list2 = [];
-    distance = 0.2;
+    // doing simplification first removes problems on turns
+    // with overlapping results
+    points = doSimplify(points);
+    
     // p1 is the 'northern' bound;  p2 is the 'southern' bound
     // of course if the track is north-south instead of east-west this
     // flips but the idea stays the same
@@ -72,10 +128,13 @@ export function getBoundingBox(points, distance) {
 	    lastP2 = p2;
 	}
     }
+    list1 = removeLoops(list1, distance);
+    list2 = removeLoops(list2, distance);
     // the last point has no bearing add it 'solo'
-    // list1.push({lat: points[points.length-1].lat, lon: points[points.length-1].lon});
+    list1.push({lat: points[points.length-1].lat, lon: points[points.length-1].lon});
     // add lower line to close loop
     list1.push.apply(list1, list2.reverse());
 
     return list1;
 }
+
